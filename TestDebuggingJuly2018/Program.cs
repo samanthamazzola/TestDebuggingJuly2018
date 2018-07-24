5682fed1f4877a9c165b8d02890422df76a70eb2
samantha mazzola <smazzola7@gmail.com>
2018-07-24 10:37:26 -0400

    added debugging notes and right and wrong if statments, the breakpoint is noted and stepped over so that the code runs; then you can see where the problem is

---
+++ TestDebuggingJuly2018/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TestDebuggingJuly2018
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a valid zip code: ");
+            string input = Console.ReadLine();
+
+            if (Regex.IsMatch(@"^\d{5}$", input))
+            {
+                Console.WriteLine("Valid input");
+            }
+            else
+            {
+                Console.WriteLine("Input not valid!");
+            }       
+        }
+    }
+}
